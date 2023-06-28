c56142b5ebceba993faa540e35ec1875c474d637
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2023-06-28 21:00:08 +0530

    try catch added

--- Backend/src/controllers/travel.controllers.js
+++ Backend/src/controllers/travel.controllers.js
@@ -9,24 +9,29 @@ router.get("", (req, res) => {
 });
 
 router.post("", (req, res) => {
-  let db = fs.readFileSync("db.json");
-  db = JSON.parse(db);
+  try {
+    let db = fs.readFileSync("db.json");
+    db = JSON.parse(db);
 
-  let existEmail = db.tours.find((e) =>{ 
-    // console.log(e.email,req.body.email)
-    return e.email.toLowerCase() === req.body.email.toLowerCase()});
+    let existEmail = db.tours.find((e) => {
+      // console.log(e.email,req.body.email)
+      return e.email.toLowerCase() === req.body.email.toLowerCase();
+    });
 
- console.log(existEmail);
+    console.log(existEmail);
 
- if(existEmail){
-  return res.status(403).send({message:"Email Already exist."})
- }
+    if (existEmail) {
+      return res.status(403).send({ message: "Email Already exist." });
+    }
 
-  let newTour = { ...req.body, id: db.tours.length + 1 };
-  db.tours.push(newTour);
-  fs.writeFileSync("db.json", JSON.stringify(db));
+    let newTour = { ...req.body, id: db.tours.length + 1 };
+    db.tours.push(newTour);
+    fs.writeFileSync("db.json", JSON.stringify(db));
 
-  res.status(201).send(newTour);
+    res.status(201).send(newTour);
+  } catch (er) {
+    return res.status(500).send({ error: er });
+  }
 });
 
 module.exports = router;
